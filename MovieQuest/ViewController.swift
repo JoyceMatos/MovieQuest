2ca7eb6e915b80f233b5a866bf117e9a487b0e9a
JoyceMatos <j.matosjoyce@gmail.com>
2016-12-26 12:49:32 -0500

    Adjust Collection View Cells

--- MovieQuest/ViewController.swift
+++ MovieQuest/ViewController.swift
@@ -8,44 +8,46 @@
 
 import UIKit
 
+
 class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
 
+    let store = OmdbDataStore.shared
+
+    private let leftAndRightPaddings: CGFloat = 10.0
+    private let numberOfItemsPerRow: CGFloat = 2.0
+    let heightAdjustment: CGFloat = 60.0
+    
+    
     @IBOutlet weak var movieView: UICollectionView!
     
-    fileprivate let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
-    fileprivate let itemsPerRow: CGFloat = 3
-
     
-    let store = OmdbDataStore.shared
-    
+    // MARK: - View Controller Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
         movieView.delegate = self
         movieView.dataSource = self
         
-//        let layout = UICollectionViewFlowLayout()
-//        layout.sectionInset = UIEdgeInsets(top: 1, left: 10, bottom: 10, right: 10)
-//        layout.minimumInteritemSpacing = 5
-//        layout.minimumLineSpacing = 10
-//        
-//        movieView.collectionViewLayout = layout
+        let width = (movieView.frame.width - leftAndRightPaddings) / numberOfItemsPerRow
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: width, height: width + heightAdjustment)
+        movieView.collectionViewLayout = layout
         
         store.getSearchedMovies(title: "spot") {
             print("Getting Called")
         }
-        
-        
+
+    }
+    
+    // MARK: - Data Source Methods
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 5
     }
-    
-    func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 1
-    }
- 
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = movieView.dequeueReusableCell(withReuseIdentifier: "movieCell", for: indexPath) as! MovieCell
@@ -60,33 +62,11 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
 
     
     
-    
-    
-    // You can always all a function for headers, ie: title = search term: "spot"
+    // You can always add a function for headers, ie: title = search term: "spot"
 
 
 
 }
 
-extension ViewController : UICollectionViewDelegateFlowLayout {
 
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        
-        let paddingSpace = sectionInsets.left * (itemsPerRow + 1)
-        let availableWidth = view.frame.width - paddingSpace
-        let widthPerItem = availableWidth / itemsPerRow
-        
-        return CGSize(width: widthPerItem, height: widthPerItem)
-    }
-
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return sectionInsets
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return sectionInsets.left
-    }
-}
 
